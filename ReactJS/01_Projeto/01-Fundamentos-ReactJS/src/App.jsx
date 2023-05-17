39a69d366d718d700d973cb89b083a84b67bf02e
warleyramires <warleyramiresgoncalves@gmail.com>
2023-05-17 01:07:55 -0300

    Componente Post

--- ReactJS/01_Projeto/01-Fundamentos-ReactJS/src/App.jsx
+++ ReactJS/01_Projeto/01-Fundamentos-ReactJS/src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./components/Header"
 import { SideBar } from "./components/SideBar"
-import Post from "./components/Post"
+import { Post } from "./components/Post"
 import styles from "./App.module.css"
 import '../src/globals.css'
 
@@ -13,14 +13,8 @@ function App() {
     <div className={styles.wrapper}>
       <SideBar/>
       <main>
-        <Post 
-          author="Warley Ramires"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vitae quis aut! Amet soluta eveniet consectetur natus architecto in atque!"  
-        />
-        <Post 
-          author="Warley Ramires"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vitae quis aut! Amet soluta eveniet consectetur natus architecto in atque!"  
-        />
+        <Post/>
+        <Post/>
         
       </main>
     </div>
--- ReactJS/01_Projeto/01-Fundamentos-ReactJS/src/components/Post.jsx
+++ ReactJS/01_Projeto/01-Fundamentos-ReactJS/src/components/Post.jsx
@@ -1,10 +1,38 @@
-function Post(props){
-    return(
-    <>
-    <h1>{props.author}</h1>
-    <p>{props.content}</p>
-    </>
-    )
-}
+import styles from "./Post.module.css";
 
-export default Post
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/48494390?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Warley Ramires</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="16 de Maio às 22:34h" dateTime="2023-05-16 22:34:21">Publicado há 1h</time>
+
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p> 
+        
+        <p><a href="#">jane.design/doctorcare</a></p> 
+        
+        <p>
+            <a href="#">#novoprojeto</a> {' '}
+            <a href="#">#nlw</a>  {' '}
+            <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
